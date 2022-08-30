b0310e0748c76af0122c182520e4d5bb6743c84a
MichaelPetersen22 <development@michaelpetersen.co>
2022-08-30 17:31:28 +1000

    Added

--- autoconfig.sh
+++ autoconfig.sh
@@ -3,9 +3,11 @@ CONFIG_DIR="~/.config"
 
 if [ $(logname) == "root" ]; then
     echo "User logged in as root, It is unsafe to run the program in root"
+    exit
 fi
 if [ $USER != "root" ]; then
     echo "Program must be run with sudo"
+    exit
 fi
 username=$(logname)
 
@@ -20,7 +22,7 @@ AUR() {
 
 Packages() {
     cat ./packages.txt | while read -r line; do
-        echo "Installing $line"
+        sudo -u $username echo "Installing $line"
         sudo -u $username yay -Sy --needed --noconfirm $line 
         echo "$line Installed"
     done
@@ -52,13 +54,13 @@ Terminal() {
 
 Config() {
     systemctl enable sddm, firewalld
-    git clone https://github.com/MichaelPetersen22/dotfiles ~/.config/dotfiles
+    sudo -u $username git clone https://github.com/MichaelPetersen22/dotfiles ~/.config/dotfiles
     mv -rf ~/.config/dotfiles/* ~/.config/
 }
 
 Styles() {
-    git clone https://github.com/vinceliuice/Fluent-gtk-theme /tmp/fluent
-    git clone https://github.com/vinceliuice/Tela-icon-theme /tmp/tela
+    sudo -u $username git clone https://github.com/vinceliuice/Fluent-gtk-theme /tmp/fluent
+    sudo -u $username git clone https://github.com/vinceliuice/Tela-icon-theme /tmp/tela
     cd /tmp/fluent; ./install.sh -c dark -n Fluent-Dark; cd -
     cd /tmp/tela; ./install.sh -c standard; cd -
 }
@@ -66,7 +68,7 @@ Styles() {
 GRUB() {
     pacman -Sy os-prober --noconfirm
     mount /dev/nvme0n1p1 /mnt
-    git clone https://github.com/vinceliuice/grub2-themes /tmp/grub
+    sudo -u $username git clone https://github.com/vinceliuice/grub2-themes /tmp/grub
     cd /tmp/grub; ./install.sh -t tela -i color -s 4k; cd -
 }
 
